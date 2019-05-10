c134582a8dab5ffd8359d1a7f792b02e81975d65
eligarlo <eligarlo.developer@gmail.com>
2019-05-10 13:23:29 +0300

    signup style and first logic build

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  registerForm: FormGroup;
+  step1 = true;
+  step2 = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      identityDocument: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      email: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      password: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      passwordConfirm: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      username: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      city: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      street: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      name: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      surname: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+    });
+  }
+
+  changeState() {
+    this.step1 = !this.step1;
+    this.step2 = !this.step2;
+  }
+
+  onRegister() {
+    console.log(this.registerForm.value);
   }
 
 }
