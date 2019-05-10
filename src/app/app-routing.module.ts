ceea5eda999610349d9c7f2c90a36f3102c498e0
eligarlo <eligarlo.developer@gmail.com>
2019-05-10 13:22:51 +0300

    routing for register added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent, pathMatch: 'full' }
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignupComponent }
 ];
 
 @NgModule({
