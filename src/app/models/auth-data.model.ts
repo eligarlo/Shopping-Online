8405b6fdec48b283de32906bf6061ff1ca070cc5
eligarlo <eligarlo.developer@gmail.com>
2019-05-23 15:14:46 +0300

    adding user model

---
+++ src/app/models/auth-data.model.ts
@@ -0,0 +1,10 @@
+export interface AuthData {
+  username: string;
+  identityDocument: string;
+  email: string;
+  password: string;
+  city: string;
+  street: string;
+  name: string;
+  surname: string;
+}
