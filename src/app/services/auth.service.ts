122f9a4b21165c2a1f3807b25ad4e50cc71c1b07
eligarlo <eligarlo.developer@gmail.com>
2019-05-23 15:13:49 +0300

    auth service added

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,114 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup } from '@angular/forms';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+const BACKEND_URL = environment.uri + 'user/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private isAuthenticated = false;
+  private token: string;
+  private userId: string;
+  private username = 'Guest';
+  private authStatusListener = new Subject<boolean>();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getIsAuth() {
+    return this.isAuthenticated;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
+
+  getUserId() {
+    return this.userId;
+  }
+
+  getUsername() {
+    return this.username;
+  }
+
+  checkUser(form) {
+    return this.http.post(BACKEND_URL + 'signupCheck', form).pipe(map(user => {
+      return user;
+    })
+    );
+  }
+
+  registerUser(form: FormGroup) {
+    return this.http.post(BACKEND_URL + 'signup', form);
+  }
+
+  login(form: FormGroup) {
+    return this.http.post<{token: string, message: string, userId: string, username: string}>(BACKEND_URL + 'login', form)
+    .pipe(map(response => {
+      this.token = response.token;
+      if (this.token) {
+        this.isAuthenticated = true;
+        this.userId = response.userId;
+        this.username = response.username;
+        this.authStatusListener.next(true);
+        this.saveAuthData(this.token, this.username, this.userId);
+      }
+      return response;
+    }));
+  }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.userId = null;
+    this.username = 'Guest';
+    this.authStatusListener.next(false);
+    this.clearAuthData();
+    this.router.navigate(['/']);
+  }
+
+  autoAuthUser() {
+    const authInfo = this.getAuthData();
+    if (!authInfo) {
+      return;
+    }
+    this.token = authInfo.token;
+    this.isAuthenticated = true;
+    this.username = authInfo.username;
+    this.userId = authInfo.userId;
+    this.authStatusListener.next(true);
+  }
+
+  private saveAuthData(token: string, username: string, userId: string) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('username', username);
+    localStorage.setItem('userId', userId);
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    localStorage.removeItem('userId');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const username = localStorage.getItem('username');
+    if (!token) {
+      return;
+    }
+    return {
+      token,
+      username,
+      userId
+    };
+  }
+}
