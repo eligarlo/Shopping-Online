b06069725a1e3c6c7f3935bdb7308a7975883030
eligarlo <eligarlo.developer@gmail.com>
2019-05-23 20:02:56 +0300

    header getting if the user is logged

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  userLogged = false;
+  userIsLogged = false;
+  username: string;
+  email: string;
+  private authListenerSub: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.userIsLogged = this.authService.getIsAuth();
+    this.username = this.authService.getUsername();
+    this.email = this.authService.getEmail();
+    this.authListenerSub = this.authService.getAuthStatusListener()
+    .subscribe(isLogged => {
+      this.userIsLogged = isLogged;
+      this.username = this.authService.getUsername();
+      this.email = this.authService.getEmail();
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authListenerSub.unsubscribe();
   }
 
 }
