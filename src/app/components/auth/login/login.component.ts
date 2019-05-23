a24cde038f9d52ed304068e329997c57faadaa3d
eligarlo <eligarlo.developer@gmail.com>
2019-05-23 15:14:22 +0300

    added login component

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { AuthService } from 'src/app/services/auth.service';
+import { AuthData } from 'src/app/models/auth-data.model';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,14 +11,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
+  alertMessage = {
+    username: '',
+    password: ''
+  };
+
+  inputsValidation = {
+    username: false,
+    password: false
+  };
+
   loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
     this.loginForm = new FormGroup({
       username: new FormControl(null, {
-        validators: [Validators.required, Validators.minLength(3)]
+        validators: [Validators.required]
       }),
       password: new FormControl(null, {
         validators: [Validators.required]
@@ -23,8 +36,31 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  onLogin() {
-    console.log(this.loginForm.value);
+  onLogin(): AuthData {
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.authService.login(this.loginForm.value).subscribe((res: any) => {
+      this.resetValues();
+      console.log(res);
+    }, err => {
+      console.log(err);
+      this.resetValues();
+      if (err.error.errUsername) {
+        this.inputsValidation.username = true;
+        this.alertMessage.username = err.error.message;
+        return;
+      }
+      this.inputsValidation.password = true;
+      this.alertMessage.password = err.error.message;
+    });
+  }
+
+  private resetValues() {
+    this.inputsValidation.username = false;
+    this.inputsValidation.password = false;
+    this.alertMessage.username = '';
+    this.alertMessage.password = '';
   }
 
 }
