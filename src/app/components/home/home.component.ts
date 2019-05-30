e1d66dfde5b8afa8c19291abf6c345ffb9ea1aa7
eligarlo <eligarlo.developer@gmail.com>
2019-05-30 14:36:28 +0300

    displaying different outputs if user or admin logged

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {Router} from '@angular/router';
+
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  userIsLogged = false;
+  role: number;
+  private authListenerSub: Subscription;
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.userIsLogged = this.authService.getIsAuth();
+    this.role = this.authService.getRole();
+    this.authListenerSub = this.authService.getAuthStatusListener()
+      .subscribe(isLogged => {
+        this.userIsLogged = isLogged;
+        this.role = this.authService.getRole();
+      });
+  }
+
+  toManagement() {
+    this.router.navigate(['/management']);
+  }
+
+  ngOnDestroy() {
+    this.authListenerSub.unsubscribe();
   }
 
 }
