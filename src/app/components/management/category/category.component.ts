c773658a73d0fa010e0e69d6ed17ab2c71c8dfca
eligarlo <eligarlo.developer@gmail.com>
2019-05-30 17:02:57 +0300

    category component now can add category

--- src/app/components/management/category/category.component.ts
+++ src/app/components/management/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CategoryService } from '../../../services/management/category.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  alertMessage = {
+    addCategory: ''
+  };
+
+  successMessage = {
+    addCategory: ''
+  };
+
+  inputsValidation = {
+    addCategory: false
+  };
+
+  successValidation = {
+    addCategory: false
+  };
+
+  addCategoryForm: FormGroup;
+
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.addCategoryForm = new FormGroup({
+      name: new FormControl(null, {validators: [Validators.required]})
+    });
+  }
+
+  onAddCategory() {
+    if (this.addCategoryForm.invalid) {
+      this.resetAllValidation();
+      this.inputsValidation.addCategory = true;
+      this.alertMessage.addCategory = 'Please enter category!';
+      return;
+    }
+    this.categoryService.addCategory(this.addCategoryForm.value).subscribe(res => {
+      if (res.result) {
+        this.resetAllValidation();
+        this.addCategoryForm.reset();
+        this.successValidation.addCategory = true;
+        this.successMessage.addCategory = res.message;
+      }
+      console.log(res);
+    });
+  }
+
+  private resetAllValidation() {
+    this.alertMessage.addCategory = '';
+    this.inputsValidation.addCategory = false;
+    this.successMessage.addCategory = '';
+    this.successValidation.addCategory = false;
   }
 
 }
