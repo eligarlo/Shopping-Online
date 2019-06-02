574e31af8881cd1972a134f0815898c6f72ec211
eligarlo <eligarlo.developer@gmail.com>
2019-06-02 16:45:26 +0300

    passing the checkManagerAuth and fileUpload middleware

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
+const checkManagerAuth = require("../middleware/check-manager-auth");
+const uploadFile = require("../middleware/fileUpload");
+
 const productController = require("../controllers/products");
 
 const router = express.Router();
 
 // In the frontend will be /api/product/addProduct
 // Saves product in the db
-router.post('/addProduct', productController.createProduct);
+router.post('/addProduct', checkManagerAuth, uploadFile, productController.createProduct);
 
 module.exports = router;
