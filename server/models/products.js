fac0d1a4b0e38fb7a7cbf49531abffef28eb0cc0
eligarlo <eligarlo.developer@gmail.com>
2019-06-02 16:38:41 +0300

    product model now getting the adminId into the Schema and adding an imagePath

--- server/models/products.js
+++ server/models/products.js
@@ -4,7 +4,9 @@ const uniqueValidator = require("mongoose-unique-validator");
 const productSchema = mongoose.Schema({
   name: { type: String, required: true, unique: true },
   price: { type: Number, required: true },
-  category: { type: String, required: true }
+  category: { type: String, required: true },
+  imagePath: { type: String, required: true },
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
 });
 
 productSchema.plugin(uniqueValidator);
