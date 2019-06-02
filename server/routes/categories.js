bbeb7e36592dae8d25e6edbc41d9b4d390c44d63
eligarlo <eligarlo.developer@gmail.com>
2019-06-02 16:44:48 +0300

    passing the checkManagerAuth middleware

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
+const checkManagerAuth = require("../middleware/check-manager-auth");
+
 const categoryController = require("../controllers/categories");
 
 const router = express.Router();
 
 // In the frontend will be /api/category/addCategory
 // Saves category in the db
-router.post('/addCategory', categoryController.createCategory);
+router.post('/addCategory', checkManagerAuth, categoryController.createCategory);
 
 // In the frontend will be /api/category/getCategories
 // Gets all the categories from the db
