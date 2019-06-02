c2b06cdf4a414a8a4b55794ac7df6ae6fc954fc5
eligarlo <eligarlo.developer@gmail.com>
2019-06-02 16:38:02 +0300

    category model now getting the adminId into the Schema

--- server/models/categories.js
+++ server/models/categories.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
 const categorySchema = mongoose.Schema({
-  name: { type: String, required: true, unique: true, uniqueCaseInsensitive: true }
+  name: { type: String, required: true, unique: true, uniqueCaseInsensitive: true },
+  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
 });
 
 categorySchema.plugin(uniqueValidator);
