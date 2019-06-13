644973e52e071bc6beeaf7e45f773c91e518a7c3
eligarlo <eligarlo.developer@gmail.com>
2019-06-13 15:14:38 +0300

    cart service updated

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { environment } from '../../environments/environment';
 
@@ -10,9 +11,40 @@ const BACKEND_URL = environment.uri + 'cart/';
 })
 export class CartService {
 
+  private cart;
+  private userMessage: string;
+  private date: number;
+
   constructor(private http: HttpClient) { }
 
-  createCart(userId) {
+  getCartFromService() {
+    return this.cart;
+  }
+
+  getUserMessage() {
+    return this.userMessage;
+  }
+
+  getDate() {
+    return new Date(this.date).toDateString();
+  }
+
+  createCart(userId: string) {
       return this.http.post(BACKEND_URL + 'addCart', {userId});
+  }
+
+  getCart(userId: string) {
+    return this.http.get<{message: string, cart: any}>(BACKEND_URL + 'getCart/' + userId)
+      .pipe(map(response => {
+        if (!response.cart) {
+          this.userMessage = response.message;
+          return false;
+        } else {
+          this.cart = response.cart;
+          this.date = response.cart[0].date;
+          this.userMessage = response.message;
+          return true;
+        }
+      }));
   }
 }
