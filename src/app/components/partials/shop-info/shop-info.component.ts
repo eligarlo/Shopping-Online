2b203f91382d2529dced8a6eca0c8ac5745798e9
eligarlo <eligarlo.developer@gmail.com>
2019-06-13 15:16:09 +0300

    updating the the message if the user is new or has cart open

--- src/app/components/partials/shop-info/shop-info.component.ts
+++ src/app/components/partials/shop-info/shop-info.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 
 import { ProductService } from '../../../services/management/product.service';
 import { AuthService } from '../../../services/auth.service';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-shop-info',
@@ -16,12 +17,21 @@ export class ShopInfoComponent implements OnInit, OnDestroy {
   userIsLogged = false;
   role: number;
   private authListenerSub: Subscription;
+  userId: string;
+  hasCart: boolean;
+  userMessage: string;
+  cartDate;
 
-  constructor(private productService: ProductService, private authService: AuthService) { }
+  constructor(private productService: ProductService,
+              private authService: AuthService,
+              private cartService: CartService) {
+
+  }
 
   ngOnInit() {
     this.userIsLogged = this.authService.getIsAuth();
     this.role = this.authService.getRole();
+    this.userId = this.authService.getUserId();
     this.productService.getProducts().subscribe(res => {
       this.totalProducts = res.products.length;
     });
@@ -29,7 +39,23 @@ export class ShopInfoComponent implements OnInit, OnDestroy {
       .subscribe(isLogged => {
         this.userIsLogged = isLogged;
         this.role = this.authService.getRole();
+        this.userId = this.authService.getUserId();
       });
+    if (this.userIsLogged && (this.role === undefined || this.role === null)) {
+      this.cartService.getCart(this.userId)
+        .subscribe(resCart => {
+            if (!resCart) {
+              this.hasCart = resCart;
+              this.userMessage = this.cartService.getUserMessage();
+            } else {
+              this.hasCart = resCart;
+              this.userMessage = this.cartService.getUserMessage();
+              this.cartDate = this.cartService.getDate();
+              console.log(this.cartDate);
+            }
+          }
+        );
+    }
   }
 
   ngOnDestroy() {
