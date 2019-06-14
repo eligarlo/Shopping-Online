2a0a156cfbbdb614c86ae6958b7ec45c9f12ed49
eligarlo <eligarlo.developer@gmail.com>
2019-06-14 17:49:46 +0300

    get cart and add product added

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+import { CartModel } from '../models/cart.model';
+
 import { environment } from '../../environments/environment';
 
 const BACKEND_URL = environment.uri + 'cart/';
@@ -14,6 +16,7 @@ export class CartService {
   private cart;
   private userMessage: string;
   private date: number;
+  private cartId: string;
 
   constructor(private http: HttpClient) { }
 
@@ -29,8 +32,12 @@ export class CartService {
     return new Date(this.date).toDateString();
   }
 
+  getCartId() {
+    return this.cartId;
+  }
+
   createCart(userId: string) {
-      return this.http.post(BACKEND_URL + 'addCart', {userId});
+      return this.http.post<{message: string, cart: CartModel}>(BACKEND_URL + 'addCart', {userId});
   }
 
   getCart(userId: string) {
@@ -42,9 +49,25 @@ export class CartService {
         } else {
           this.cart = response.cart[0];
           this.date = response.cart[0].date;
+          this.cartId = response.cart[0]._id;
           this.userMessage = response.message;
           return true;
         }
       }));
   }
+
+  getCartByCartId(cartId: string) {
+    return this.http.get<{message: string, cart: CartModel}>(BACKEND_URL + 'getByCartId/' + cartId)
+      .pipe(map( response => {
+        this.cart = response.cart[0];
+        return true;
+      }));
+  }
+
+  addToCart(product: any) {
+    const productData = product;
+    productData.cartId = this.cartId;
+    return this.http.post(BACKEND_URL + 'addProduct', productData);
+  }
+
 }
