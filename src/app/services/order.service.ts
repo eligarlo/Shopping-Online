c9fbb8b3e908ad49171949c78a7c881d83c9d502
eligarlo <eligarlo.developer@gmail.com>
2019-06-18 19:15:57 +0300

    getting the shipment details for the user in the front end

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../environments/environment';
+
+const BACKEND_URL = environment.uri + 'order/';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(private http: HttpClient) { }
+
+  getShipmentDetails(userId: string) {
+    return this.http.get(BACKEND_URL + 'getShipmentDetails/' + userId);
+  }
+}
