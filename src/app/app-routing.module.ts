6e0f72b2417bff6e14598c34cf65adca6e45fac8
eligarlo <eligarlo.developer@gmail.com>
2019-06-18 12:22:27 +0300

    order component added to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 // Guards
-import {AuthGuard} from './guards/auth.guard';
-import {ManagementGuard} from './guards/management.guard';
+import { AuthGuard } from './guards/auth.guard';
+import { ManagementGuard } from './guards/management.guard';
 
 // Components
-import {HomeComponent} from './components/home/home.component';
-import {HomeLoginComponent} from './components/home-login/home-login.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeLoginComponent } from './components/home-login/home-login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
-import {ManagementComponent} from './components/management/management.component';
-import {ShopComponent} from './components/shop/shop.component';
+import { ManagementComponent } from './components/management/management.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { OrderComponent } from './components/order/order.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: HomeLoginComponent },
   { path: 'register', component: SignupComponent },
   { path: 'management', component: ManagementComponent, canActivate: [ManagementGuard] },
-  { path: 'shop/:cartId', component: ShopComponent, canActivate: [AuthGuard] }
+  { path: 'shop/:cartId', component: ShopComponent, canActivate: [AuthGuard] },
+  { path: 'order/:cartId', component: OrderComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
