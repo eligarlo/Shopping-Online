d9ae00d27bf8dd436fdaf950b13fc4dfb8fa0907
eligarlo <eligarlo.developer@gmail.com>
2019-07-01 12:19:33 +0300

    error interceptor created

---
+++ src/app/interceptors/error-interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(req).pipe(
+      retry(1),
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = 'An unknown error occurred!';
+        if (error.error.message) {
+          errorMessage = error.error.message;
+        }
+        window.alert(errorMessage);
+        return throwError(error);
+      })
+    );
+  }
+}
